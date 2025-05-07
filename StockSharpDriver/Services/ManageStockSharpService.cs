e865506d74b49508f7585e9cfd63fbe371deab03
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-05-07 09:48:02 +0300

    ~

--- StockSharpDriver/Services/ManageStockSharpService.cs
+++ StockSharpDriver/Services/ManageStockSharpService.cs
@@ -13,18 +13,21 @@ public class ManageStockSharpService(IDbContextFactory<StockSharpAppContext> too
     /// <inheritdoc/>
     public async Task<TResponseModel<int>> UpdateOrCreateAdapterAsync(FixMessageAdapterModel req, CancellationToken cancellationToken = default)
     {
+        StockSharpAppContext ctx = await toolsDbFactory.CreateDbContextAsync(cancellationToken);
         throw new NotImplementedException();
     }
 
     /// <inheritdoc/>
     public async Task<TPaginationResponseModel<FixMessageAdapterModel>> AdaptersSelectAsync(TPaginationRequestStandardModel<AdaptersRequestModel> req, CancellationToken cancellationToken = default)
     {
+        StockSharpAppContext ctx = await toolsDbFactory.CreateDbContextAsync(cancellationToken);
         throw new NotImplementedException();
     }
 
     /// <inheritdoc/>
     public async Task<ResponseBaseModel> DeleteAdapterAsync(FixMessageAdapterModel req, CancellationToken cancellationToken = default)
     {
+        StockSharpAppContext ctx = await toolsDbFactory.CreateDbContextAsync(cancellationToken);
         throw new NotImplementedException();
     }
 }
