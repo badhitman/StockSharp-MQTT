95434df8813d2cab2352092516d38fadbf2b99cf
Stas Uspenskiy <ru.usa.goo@gmail.com>
2025-06-18 17:09:50 +0300

    ~

---
+++ StockSharpMauiApp/Components/Shared/ProgressIndicatorComponent.razor.cs
@@ -0,0 +1,77 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+using BlazorLib;
+using Microsoft.AspNetCore.Components;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Server;
+using SharedLib;
+using System.Text;
+
+namespace StockSharpMauiApp.Components.Shared;
+
+public partial class ProgressIndicatorComponent : BlazorBusyComponentBaseModel
+{
+    [Inject]
+    StockSharpClientConfigModel rabbitConf { get; set; } = default!;
+
+    [Inject]
+    IServiceProvider servicesProvider { get; set; } = default!;
+
+    IMqttClient mqttClient = default!;
+    MqttFactory mqttFactory = new();
+
+    int rotateDeg;
+    int _counter;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        mqttClient = mqttFactory.CreateMqttClient();
+        mqttClient.ApplicationMessageReceivedAsync += ApplicationMessageReceived;
+        await mqttClient.ConnectAsync(GetMqttClientOptionsBuilder);
+        await mqttClient.SubscribeAsync("#");
+    }
+
+    Task ApplicationMessageReceived(MqttApplicationMessageReceivedEventArgs e)
+    {
+        //string content = Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment).Trim();
+        rotateDeg += 5;
+        if (rotateDeg >= 360)
+        {
+            rotateDeg = 0;
+            _counter++;
+        }
+
+        StateHasChangedCall();
+        return Task.CompletedTask;
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        using IServiceScope scope = servicesProvider.CreateScope();
+        mqttClient = mqttFactory.CreateMqttClient();
+
+    }
+
+    public override void Dispose()
+    {
+        mqttClient.Dispose();
+        mqttClient.ApplicationMessageReceivedAsync -= ApplicationMessageReceived;
+        base.Dispose();
+    }
+
+    MqttClientOptions GetMqttClientOptionsBuilder
+    {
+        get
+        {
+            return new MqttClientOptionsBuilder()
+               .WithTcpServer(rabbitConf.Host, rabbitConf.Port)
+               .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
+               .Build();
+        }
+    }
+}
