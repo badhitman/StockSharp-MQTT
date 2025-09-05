9ce3f9341ca5d5ae634d3f76841e382257aa7434
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-09-05 14:24:36 +0300

    ~

---
+++ StockSharpDriver/Services/TelegramBot/Abstract/ITelegramBotService.cs
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+using SharedLib;
+
+namespace StockSharpDriver;
+
+/// <summary>
+/// TelegramBot
+/// </summary>
+public interface ITelegramBotService : ITelegramBotStandardService
+{
+    /// <summary>
+    /// ChatsFindForUserTelegram
+    /// </summary>
+    public Task<List<ChatTelegramModelDB>> ChatsFindForUserTelegramAsync(long[] req, CancellationToken token = default);
+
+    /// <summary>
+    /// ChatsReadTelegram
+    /// </summary>
+    public Task<List<ChatTelegramModelDB>> ChatsReadTelegramAsync(long[] req, CancellationToken token = default);
+
+    /// <summary>
+    /// ChatsSelectTelegram
+    /// </summary>
+    public Task<TPaginationResponseModel<ChatTelegramModelDB>> ChatsSelectTelegramAsync(TPaginationRequestModel<string?> req, CancellationToken token = default);
+
+    /// <summary>
+    /// ChatTelegramRead
+    /// </summary>
+    public Task<ChatTelegramModelDB> ChatTelegramReadAsync(int chatId, CancellationToken token = default);
+
+    /// <summary>
+    /// ErrorsForChatsSelectTelegram
+    /// </summary>
+    public Task<TPaginationResponseModel<ErrorSendingMessageTelegramBotModelDB>> ErrorsForChatsSelectTelegramAsync(TPaginationRequestModel<long[]> req, CancellationToken token = default);
+
+    /// <summary>
+    /// ForwardMessageTelegram
+    /// </summary>
+    public Task<TResponseModel<MessageComplexIdsModel>> ForwardMessageTelegramAsync(ForwardMessageTelegramBotModel req, CancellationToken token = default);
+
+    /// <summary>
+    /// GetFileTelegram
+    /// </summary>
+    public Task<TResponseModel<byte[]>> GetFileTelegramAsync(string req, CancellationToken token = default);
+
+    /// <summary>
+    /// MessagesSelectTelegram
+    /// </summary>
+    public Task<TPaginationResponseModel<MessageTelegramModelDB>> MessagesSelectTelegramAsync(TPaginationRequestModel<SearchMessagesChatModel> req, CancellationToken token = default);
+}
